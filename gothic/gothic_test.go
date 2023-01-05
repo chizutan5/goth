3663a5378d2a43eb4b1a7c86719226336b1b6a5d
chizutan5 <68605288+chizutan5@users.noreply.github.com>
2023-01-06 04:44:01 +0900

    Update gothic_test.go

--- gothic/gothic_test.go
+++ gothic/gothic_test.go
@@ -13,9 +13,9 @@ import (
 	"testing"
 
 	"github.com/gorilla/sessions"
-	"github.com/markbates/goth"
-	. "github.com/markbates/goth/gothic"
-	"github.com/markbates/goth/providers/faux"
+	"github.com/chizutan5/goth"
+	. "github.com/chizutan5/goth/gothic"
+	"github.com/chizutan5/goth/providers/faux"
 	"github.com/stretchr/testify/assert"
 )
 
